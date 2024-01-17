eee5b42e05f37348d2b15b4af1c8816edde26567
Kevin <kevincccstudent@gmail.com>
2024-01-16 22:54:56 -0800

    Done

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+let horizon = window.innerHeight / 1.75;
+let heightOfSky = window.innerHeight-horizon;
+let heightOfGrass = horizon;
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+function tile(url, left, bottom, width, height) {
+    for (let h = 0; h < height; h++) {
+        for (let w = 0; w < width; w++) {
+            newImage(url, left + w * 100, bottom + h * 100)
+        }
+    }
+}
+
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -8,12 +25,19 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function item(url, left, bottom) {
+    let object = newImage(url, left, bottom)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
-function newInventory(){
-    let inventory = document.createElement('div')
+let inventory = document.createElement('div')
+
+function newInventory() {
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -26,6 +50,7 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return 
 }
 
 newInventory()
@@ -36,6 +61,7 @@ newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+item('assets/sword.png', 500, 555)
+item('assets/shield.png', 185, 335)
+item('assets/staff.png', 600, 250)
+
